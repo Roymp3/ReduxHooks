689a39facdd0957069f63e865464d4e70d257db4
Roymp3 <71888012+Roymp3@users.noreply.github.com>
2021-04-15 12:05:56 -0300

    Add files via upload

---
+++ reduxshooks/src/components/CourseList/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux'
+
+
+function addCourseAction(title){
+    return {type: 'ADD_COURSE', title}
+}
+
+function CourseList() {
+
+    const qty = 2
+
+  const courses = useSelector(
+    state => state.data.slice(0, qty),
+    [qty]
+  );
+
+
+  const dispatch = useDispatch();
+  function addCourse(){
+    dispatch(addCourseAction('GraphQl'))
+  }
+
+  return (
+      <>
+        <ul>
+            {courses.map(course => <li key={course}>{course}</li>)}
+         </ul>
+
+        <button type="button" onClick={addCourse}>
+            Adcionar cursos
+        </button>
+    </>
+  );
+}
+
+export default CourseList;
